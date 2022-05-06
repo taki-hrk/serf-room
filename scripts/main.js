53666e747c5c1424811a71a0518501267c292c3c
柊 <104633913+hiiragi-iwashi@users.noreply.github.com>
2022-05-07 07:09:08 +0900

    name-button discard
    ユーザーネーム変更ボタン及び関連スクリプトの消去

--- scripts/main.js
+++ scripts/main.js
@@ -9,33 +9,3 @@ myImage.onclick = function () {
         myImage.setAttribute('src','images/fox.jpg');
     }
 }
-
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-/*
-function setUserName() {
-    let myName = prompt('あなたの名前を入力してください。');
-    if (!myName) {
-        setUserName();
-    }
-    else{
-        localStorage.setItem('name', myName);
-        myHeading.textContent = '愛です 愛ですよ ' + myName;
-    }
-    
-}
-
-if (!localStorage.getItem('name')) {
-    setUserName();
-}
-else{
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = storedName + 'の自習室';
-}
-
-myButton.onclick = function () {
-    setUserName();
-}
-*/
